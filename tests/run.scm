e2e586c8f746ce65dcaa124ad2f327d59aaff040
Peter Danenberg <pcd@roxygen.org>
2012-05-25 19:00:01 -0700

    Add some trivial tests for replace.

---
+++ tests/run.scm
@@ -0,0 +1,15 @@
+(use neo4j test)
+
+(test
+ "a b c"
+ (replace "a {x} c" "x" "b"))
+
+(test
+ "a b {y} d"
+ (replace "a {x} {y} d" "x" "b"))
+
+(test
+ "a b c d"
+ (replace "a {x} {y} d"
+          '(("x" "b")
+            ("y" "c"))))
